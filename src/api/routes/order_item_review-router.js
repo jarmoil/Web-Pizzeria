e8fa633a2e9c992f2889f384e7842e8ac6a6fea2
Otto <ottoparnanen1@gmail.com>
2025-04-21 13:38:10 +0300

    Update menu-model to search items with correct id

--- src/api/routes/order_item_review-router.js
+++ src/api/routes/order_item_review-router.js
@@ -2,10 +2,10 @@ import express from 'express';
 import {
   postOrderItemReview,
   fetchOrderItemReviews,
-  deleteOrderItemReviewController
+  deleteOrderItemReviewController,
 } from '../controllers/order_item_review-controller.js';
 
-import { authenticateToken } from '../middleware/auth-middleware.js';
+import {authenticateToken} from '../middleware/auth-middleware.js';
 
 const reviewRouter = express.Router();
 
@@ -16,6 +16,7 @@ reviewRouter
 
 reviewRouter
   .route('/:id')
+  .get()
   .delete(authenticateToken, deleteOrderItemReviewController);
 
 export default reviewRouter;
