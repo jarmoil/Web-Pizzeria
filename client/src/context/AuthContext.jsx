7d8fda668e1f93fd6314f83001a2b3a8e2437c2e
xTheKn1ght <aaron.lyhtinen@gmail.com>
2025-05-06 14:43:31 +0300

    Lisäsin id authcontextiin

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -21,7 +21,9 @@ export const AuthProvider = ({children}) => {
     const token = getToken();
     if (token) {
       const decoded = decodeToken(token);
-      decoded && setUser({token, role: decoded.role});
+      decoded && setUser({token,
+        userId: user_id,
+        role: decoded.role});
     }
   }, []);
 
@@ -31,7 +33,9 @@ export const AuthProvider = ({children}) => {
       const {token} = await loginUser(credentials);
       setToken(token);
       const decoded = decodeToken(token);
-      decoded && setUser({token, role: decoded.role});
+      decoded && setUser({token,
+        userId: user_id,
+        role: decoded.role});
     } catch (err) {
       setError(err.response?.data?.message || 'Login failed');
     } finally {
